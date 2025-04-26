89b07ad51a25a91ba39f228d182479caec0c2795
supro <suprojitmallick2006@gmail.com>
2025-04-26 10:36:50 +0530

    added deletion on max heap

--- MaxHeap.c
+++ MaxHeap.c
@@ -1,8 +1,9 @@
+//deletion and insertion on max heap
 #include<stdio.h>
 #include<stdlib.h>
 #include<math.h>
 int *arr;
-int *maxheap(int *a,int pos)
+int *InsertMaxHeap(int *a,int pos)
 {
     int i=pos;
     while(i)
@@ -23,6 +24,54 @@ int *maxheap(int *a,int pos)
     }
     return a;
 }
+int *DeleteMaxHeap(int *a,int size)
+{
+    int temp;
+    temp=a[0];
+    a[0]=a[size-1];
+    a[size-1]=temp;
+    int i=0;
+    int left,right;
+    while(i<size-1)
+    {
+        left=(i*2)+1;
+        right=(i*2)+2;
+        if(right<size-1)
+        {
+            if(a[right]>a[left] && a[right]>a[i])
+            {
+                temp=a[i];
+                a[i]=a[right];
+                a[right]=temp;
+                i=right;
+            }
+            else if(a[left]>a[right] && a[left]>a[i])
+            {
+                temp=a[i];
+                a[i]=a[left];
+                a[left]=temp;
+                i=left;
+            }
+            else
+                break;
+        }  
+        else if(left<size-1)
+        {
+            if(a[left]>a[i])
+            {
+                temp=a[i];
+                a[i]=a[left];
+                a[left]=temp;
+                i=left;
+            }
+            else
+                break;
+        }
+        else
+            break;       
+    }
+    return a;
+}
 int main()
 {
     printf("enter the number of inputs:");
@@ -33,7 +82,21 @@ int main()
     {
         printf("Enter the element %d: ",i+1);
         scanf("%d",&arr[i]);
-        maxheap(arr,i);
+        InsertMaxHeap(arr,i);
+    }
+    printf("ARRAY Representation :");
+    for(int i=0;i<size;i++)
+    {
+        printf("%d ",arr[i]);
+    }
+    printf("\n");
+    printf("Do you want to delete element[1/0] : ");
+    int choice;
+    scanf("%d",&choice);
+    if(choice)
+    {
+        DeleteMaxHeap(arr,size);
+        size--;
     }
     printf("ARRAY Representation :");
     for(int i=0;i<size;i++)
